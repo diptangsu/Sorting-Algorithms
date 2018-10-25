c7a6c2579e7bf052787fbb19bc02a2dd69732543
Diptangsu Goswami <deep.dippu@gmail.com>
2018-10-25 17:21:13 +0530

    Update BucketSort.java

--- src/BucketSort.java
+++ src/BucketSort.java
@@ -1,12 +1,12 @@
 import java.util.Random;
  
-public class Bucket_Sort 
+class BucketSort 
 {
-    static int[] sort(int[] sequence, int maxValue) 
+    static int[] bucketSort(int[] sequence, int maxValue) 
     {
         // Bucket Sort
         int[] Bucket = new int[maxValue + 1];
-        int[] sorted_sequence = new int[sequence.length];
+        int[] sortedSequence = new int[sequence.length];
  
         for (int i = 0; i < sequence.length; i++)
             Bucket[sequence[i]]++;
@@ -14,15 +14,15 @@ public class Bucket_Sort
         int outPos = 0;
         for (int i = 0; i < Bucket.length; i++)
             for (int j = 0; j < Bucket[i]; j++)
-                sorted_sequence[outPos++] = i;
+                sortedSequence[outPos++] = i;
  
-        return sorted_sequence;
+        return sortedSequence;
     }
  
-    static void printSequence(int[] sorted_sequence) 
+    static void printSequence(int[] sortedSequence) 
     {
-        for (int i = 0; i < sorted_sequence.length; i++)
-            System.out.print(sorted_sequence[i] + " ");
+        for (int i = 0; i < sortedSequence.length; i++)
+            System.out.print(sortedSequence[i] + " ");
     }
  
     static int maxValue(int[] sequence) 
@@ -36,8 +36,7 @@ public class Bucket_Sort
  
     public static void main(String args[]) 
     {
-        System.out
-                .println("Sorting of randomly generated numbers using BUCKET SORT");
+        System.out.println("Sorting of randomly generated numbers using BUCKET SORT");
         Random random = new Random();
         int N = 20;
         int[] sequence = new int[N];
@@ -51,6 +50,6 @@ public class Bucket_Sort
         printSequence(sequence);
  
         System.out.println("\nSorted Sequence: ");
-        printSequence(sort(sequence, maxValue));
+        printSequence(bucketSort(sequence, maxValue));
     }
 }
