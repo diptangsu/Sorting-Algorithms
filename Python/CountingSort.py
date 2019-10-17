5a40f3b70c0044f72b3b181477eabd516fc777f4
diptangsu <deep.dippu@gmail.com>
2019-10-17 23:05:41 +0530

    fixed formatting

--- Python/CountingSort.py
+++ Python/CountingSort.py
@@ -4,18 +4,17 @@
 # alphabetical order
 
 
-def countSort(arr):
-
+def count_sort(arr):
     # The output character array that will have sorted arr
-    output = [0 for i in range(256)]
+    output = [0 for _ in range(256)]
 
     # Create a count array to store count of inidividul
     # characters and initialize count array as 0
-    count = [0 for i in range(256)]
+    count = [0 for _ in range(256)]
 
     # For storing the resulting answer since the
     # string is immutable
-    ans = ["" for _ in arr]
+    ans = ['' for _ in arr]
 
     # Store count of each character
     for i in arr:
@@ -24,11 +23,11 @@ def countSort(arr):
     # Change count[i] so that count[i] now contains actual
     # position of this character in output array
     for i in range(256):
-        count[i] += count[i-1]
+        count[i] += count[i - 1]
 
     # Build the output character array
     for i in range(len(arr)):
-        output[count[ord(arr[i])]-1] = arr[i]
+        output[count[ord(arr[i])] - 1] = arr[i]
         count[ord(arr[i])] -= 1
 
     # Copy the output array to arr, so that arr now
@@ -39,6 +38,6 @@ def countSort(arr):
 
 
 # Driver program to test above function
-arr = "geeksforgeeks"
-ans = countSort(arr)
-print "Sorted character array is %s" % ("".join(ans))
+arr = "mynameisdiptangsugoswami"
+ans = count_sort(arr)
+print(f'Sorted character array is: {"".join(ans)}')
