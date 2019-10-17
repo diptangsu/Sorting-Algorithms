46c129f9d0e63cca61431601a5bdbc43be0d39fa
diptangsu <deep.dippu@gmail.com>
2019-10-17 22:27:30 +0530

    fixed formatting

--- Python/BitonicSort.py
+++ Python/BitonicSort.py
@@ -5,7 +5,7 @@
 # The parameter dir indicates the sorting direction, ASCENDING
 # or DESCENDING; if (a[i] > a[j]) agrees with the direction,
 # then a[i] and a[j] are interchanged.*/
-def compAndSwap(a, i, j, dire):
+def comp_and_swap(a, i, j, dire):
     if (dire == 1 and a[i] > a[j]) or (dire == 0 and a[i] < a[j]):
         a[i], a[j] = a[j], a[i]
 
@@ -14,30 +14,30 @@ def compAndSwap(a, i, j, dire):
 # if dir = 1, and in descending order otherwise (means dir=0).
 # The sequence to be sorted starts at index position low,
 # the parameter cnt is the number of elements to be sorted.
-def bitonicMerge(a, low, cnt, dire):
+def bitonic_merge(a, low, cnt, dire):
     if cnt > 1:
-        k = cnt/2
-        for i in range(low, low+k):
-            compAndSwap(a, i, i+k, dire)
-        bitonicMerge(a, low, k, dire)
-        bitonicMerge(a, low+k, k, dire)
+        k = cnt // 2
+        for i in range(low, low + k):
+            comp_and_swap(a, i, i + k, dire)
+        bitonic_merge(a, low, k, dire)
+        bitonic_merge(a, low + k, k, dire)
 
 
 # This funcion first produces a bitonic sequence by recursively
 # sorting its two halves in opposite sorting orders, and then
-# calls bitonicMerge to make them in the same order
-def bitonicSort(a, low, cnt, dire):
+# calls bitonic_merge to make them in the same order
+def bitonic_sort(a, low, cnt, dire):
     if cnt > 1:
-        k = cnt/2
-        bitonicSort(a, low, k, 1)
-        bitonicSort(a, low+k, k, 0)
-        bitonicMerge(a, low, cnt, dire)
+        k = cnt // 2
+        bitonic_sort(a, low, k, 1)
+        bitonic_sort(a, low + k, k, 0)
+        bitonic_merge(a, low, cnt, dire)
 
 
-# Caller of bitonicSort for sorting the entire array of length N
+# Caller of bitonic_sort for sorting the entire array of length n
 # in ASCENDING order
-def sort(a, N, up):
-    bitonicSort(a, 0, N, up)
+def sort(a, n, up):
+    bitonic_sort(a, 0, n, up)
 
 
 # Driver code to test above
@@ -45,7 +45,6 @@ a = [3, 7, 4, 8, 6, 2, 1, 5]
 n = len(a)
 up = 1
 
+print('Array:', a)
 sort(a, n, up)
-print("\n\nSorted array is")
-for i in range(n):
-    print("%d" % a[i])
+print('Sorted array:', a)
