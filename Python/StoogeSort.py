d0ea400464ec788c3e793b29eff26ff564d32db2
diptangsu <deep.dippu@gmail.com>
2019-10-18 00:05:31 +0530

    fixed formatting

--- Python/StoogeSort.py
+++ Python/StoogeSort.py
@@ -1,6 +1,4 @@
 # Python program to implement stooge sort
-
-
 def stoogesort(arr, l, h):
     if l >= h:
         return
@@ -14,25 +12,24 @@ def stoogesort(arr, l, h):
 
     # If there are more than 2 elements in
     # the array
-    if h-l + 1 > 2:
-        t = (int)((h-l + 1)/3)
+    if h - l + 1 > 2:
+        t = (h - l + 1) // 3
 
         # Recursively sort first 2 / 3 elements
-        stoogesort(arr, l, (h-t))
+        stoogesort(arr, l, (h - t))
 
         # Recursively sort last 2 / 3 elements
         stoogesort(arr, l + t, (h))
 
         # Recursively sort first 2 / 3 elements
         # again to confirm
-        stoogesort(arr, l, (h-t))
+        stoogesort(arr, l, (h - t))
 
 
 # deriver
 arr = [2, 4, 5, 3, 1]
 n = len(arr)
 
-stoogesort(arr, 0, n-1)
+stoogesort(arr, 0, n - 1)
 
-for i in range(0, n):
-    print(arr[i], end=' ')
+print(arr)
