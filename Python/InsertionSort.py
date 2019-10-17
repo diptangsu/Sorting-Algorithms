46c3726e407e0c8ced094418faf555ccdedc372d
diptangsu <deep.dippu@gmail.com>
2019-10-17 23:21:03 +0530

    fixed formatting for timsort

--- Python/InsertionSort.py
+++ Python/InsertionSort.py
@@ -1,19 +1,15 @@
 # Python program for implementation of Insertion Sort
 
 # Function to do insertion sort
-
-
-def insertionSort(arr):
-
+def insertion_sort(arr):
     # Traverse through 1 to len(arr)
     for i in range(1, len(arr)):
-
         key = arr[i]
 
         # Move elements of arr[0..i-1], that are
         # greater than key, to one position ahead
         # of their current position
-        j = i-1
+        j = i - 1
         while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
@@ -22,6 +18,5 @@ def insertionSort(arr):
 
 # Driver code to test above
 arr = [12, 11, 13, 5, 6]
-insertionSort(arr)
-for i in range(len(arr)):
-    print("% d" % arr[i])
+insertion_sort(arr)
+print(arr)
