f9c014730d340b3472c72583e7e45321553072e5
diptangsu <deep.dippu@gmail.com>
2019-10-17 23:20:21 +0530

    fixed formatting for timsort

--- Python/TimSort.py
+++ Python/TimSort.py
@@ -5,24 +5,20 @@ RUN = 32
 # to right index which is of size atmost RUN
 
 
-def insertionSort(arr, left, right):
-
-    for i in range(left + 1, right+1):
-
+def insertion_sort(arr, left, right):
+    for i in range(left + 1, right + 1):
         temp = arr[i]
         j = i - 1
-        while arr[j] > temp and j >= left:
 
-            arr[j+1] = arr[j]
+        while arr[j] > temp and j >= left:
+            arr[j + 1] = arr[j]
             j -= 1
 
-        arr[j+1] = temp
+        arr[j + 1] = temp
+
 
 # merge function merges the sorted runs
-
-
 def merge(arr, l, m, r):
-
     # original array is broken in two parts
     # left and right array
     len1, len2 = m - l + 1, r - m
@@ -36,11 +32,9 @@ def merge(arr, l, m, r):
     # after comparing, we merge those two array
     # in larger sub array
     while i < len1 and j < len2:
-
         if left[i] <= right[j]:
             arr[k] = left[i]
             i += 1
-
         else:
             arr[k] = right[j]
             j += 1
@@ -49,7 +43,6 @@ def merge(arr, l, m, r):
 
     # copy remaining elements of left, if any
     while i < len1:
-
         arr[k] = left[i]
         k += 1
         i += 1
@@ -64,53 +57,38 @@ def merge(arr, l, m, r):
 # array[0...n-1] (similar to merge sort)
 
 
-def timSort(arr, n):
-
+def tim_sort(arr):
+    n = len(arr)
     # Sort individual subarrays of size RUN
     for i in range(0, n, RUN):
-        insertionSort(arr, i, min((i+31), (n-1)))
+        insertion_sort(arr, i, min((i + 31), (n - 1)))
 
     # start merging from size RUN (or 32). It will merge
     # to form size 64, then 128, 256 and so on ....
     size = RUN
     while size < n:
-
         # pick starting point of left sub array. We
         # are going to merge arr[left..left+size-1]
         # and arr[left+size, left+2*size-1]
         # After every merge, we increase left by 2*size
-        for left in range(0, n, 2*size):
-
+        for left in range(0, n, 2 * size):
             # find ending point of left sub array
             # mid+1 is starting point of right sub array
             mid = left + size - 1
-            right = min((left + 2*size - 1), (n-1))
+            right = min((left + 2 * size - 1), (n - 1))
 
             # merge sub array arr[left.....mid] &
             # arr[mid+1....right]
             merge(arr, left, mid, right)
 
-        size = 2*size
-
-# utility function to print the Array
-
-
-def printArray(arr, n):
-
-    for i in range(0, n):
-        print(arr[i], end=" ")
-    print()
+        size = 2 * size
 
 
 # Driver program to test above function
 if __name__ == "__main__":
-
     arr = [5, 21, 7, 23, 19]
-    n = len(arr)
-    print("Given Array is")
-    printArray(arr, n)
+    print("Given Array is:", arr)
 
-    timSort(arr, n)
+    tim_sort(arr)
 
-    print("After Sorting Array is")
-    printArray(arr, n)
+    print("After Sorting Array is:", arr)
