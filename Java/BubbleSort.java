531604a8872dca76a0b0af8116513de5331ee671
Febri D. Rianto <44501367+Blazing21@users.noreply.github.com>
2019-10-22 19:23:02 +0800

    Update BubbleSort.java

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
-public class BubbleSort {
+public class coba {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("BUBBLE SORT");
         System.out.print("Enter the number of elements : ");
         int n = sc.nextInt();
         int arr[] = new int[n];
         System.out.println("Enter " + n + " elements :");
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
-
+        System.out.print("Before sorted array : ");
+        for(int x = 0; x < n; x++)
+            System.out.print(arr[x]+" ");
         bubbleSort(arr);
-
-        System.out.println("\nThe sorted array : ;");
+        System.out.print("\nAfter sorted array : ");
         for (int i = 0; i < n; i++)
             System.out.print(arr[i] + " ");
         System.out.println();
