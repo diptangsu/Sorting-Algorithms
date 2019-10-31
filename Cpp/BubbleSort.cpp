c9bd2d6ce51e14d1b923e28d6757400386781546
Arathy Rose <arathyrose2000@gmail.com>
2019-10-31 15:40:17 +0530

    Fixed issue #89 in C++ language by breaking the outer loop if the inner loop does not swap any element

--- Cpp/BubbleSort.cpp
+++ Cpp/BubbleSort.cpp
@@ -11,13 +11,20 @@ void swap(int *xp, int *yp)
 // A function to implement bubble sort 
 void bubbleSort(int arr[], int n) 
 { 
-	int i, j; 
+	int i, j, is_swap = 0; 
 	for (i = 0; i < n-1; i++)	 
-	
-	// Last i elements are already in place 
-	for (j = 0; j < n-i-1; j++) 
-		if (arr[j] > arr[j+1]) 
-			swap(&arr[j], &arr[j+1]); 
+	{
+		is_swap=0;
+		// Last i elements are already in place 
+		for (j = 0; j < n-i-1; j++) 
+			if (arr[j] > arr[j+1]) 
+			{	
+				swap(&arr[j], &arr[j+1]); 
+				is_swap=1;
+			}
+		if(is_swap==0)
+			break;
+	}
 } 
 
 /* Function to print an array */
