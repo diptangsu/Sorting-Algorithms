0adf26ad01f5ecb5cb74949ac31218dd13e87358
suraj0223 <50772234+suraj0223@users.noreply.github.com>
2020-10-01 02:17:18 +0530

    Updates and Renamed file to ThreeWayQuickSort.cpp

--- Cpp/ThreeWayQuickSort.cpp
+++ Cpp/ThreeWayQuickSort.cpp
@@ -46,15 +46,15 @@ void randomized_quick_sort(vector<int> &v1, int l, int r) {
 int main() {
 
   int n;
-  std::cin >> n;
+  cin >> n;
   vector<int> a(n);
   for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
+    cin >> a[i];
   }
   randomized_quick_sort(a, 0, a.size() - 1);
 
   for (size_t i = 0; i < a.size(); ++i) {
-    std::cout << a[i] << ' ';
+    cout << a[i] << ' ';
   }
   return 0;
 }
