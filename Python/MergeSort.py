dd068fd4de0fd1e1231ab8c0fa139f9e9a4365d1
CiTRuS93 <hadarradah@gmail.com>
2020-09-30 17:51:18 +0300

    pep 8 fix

--- Python/MergeSort.py
+++ Python/MergeSort.py
@@ -1,12 +1,12 @@
 # Python program for implementation of MergeSort
 # Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
+# First subarray is arr[l..middle]
+# Second subarray is arr[middle+1..right]
 
 
-def merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r - m
+def merge(arr, left, middle, right):
+    n1 = middle - left + 1
+    n2 = right - middle
 
     # create temp arrays
     L = [0] * (n1)
@@ -14,15 +14,15 @@ def merge(arr, l, m, r):
 
     # Copy data to temp arrays L[] and R[]
     for i in range(0, n1):
-        L[i] = arr[l + i]
+        L[i] = arr[left + i]
 
     for j in range(0, n2):
-        R[j] = arr[m + 1 + j]
+        R[j] = arr[middle + 1 + j]
 
-    # Merge the temp arrays back into arr[l..r]
+    # Merge the temp arrays back into arr[left..right]
     i = 0     # Initial index of first subarray
     j = 0     # Initial index of second subarray
-    k = l     # Initial index of merged subarray
+    k = left     # Initial index of merged subarray
 
     while i < n1 and j < n2:
         if L[i] <= R[j]:
