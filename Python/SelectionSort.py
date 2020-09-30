39f8a2ef2d8d2d90acc77ee94a84bc5baf3d688f
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2020-10-01 00:11:36 +0530

    Update SelectionSort.py

--- Python/SelectionSort.py
+++ Python/SelectionSort.py
@@ -1,18 +1,30 @@
 # Python program for implementation of Selection Sort
-a = [64, 25, 12, 22, 11]
+def selectnsort(a):
 
-# Traverse through all array elements
-for i in range(len(a)):
-    # Find the minimum element in remaining
-    # unsorted array
-    min_idx = i
-    for j in range(i + 1, len(a)):
-        if a[min_idx] > a[j]:
-            min_idx = j
+    # Traverse through all array elements
+    for i in range(len(a)):
+        # Find the minimum element in remaining
+        # unsorted array
+        min_idx = i
+        for j in range(len(a)):
+            if a[min_idx] < a[j]:
+                min_idx = j
 
-    # Swap the found minimum element with
-    # the first element
-    a[i], a[min_idx] = a[min_idx], a[i]
+            # Swap the found minimum element with
+            # the first element
+            a[i], a[min_idx] = a[min_idx], a[i]
 
-# Driver code to test above
-print("Sorted array", a)
+    return a
+
+if __name__ == "__main__":
+    print("Enter the number of elements:")
+    n = int(input())
+    print("Enter the elements of array:")
+    a = []
+    for i in range(n):
+        x = int(input())
+        a.append(x)
+    # Driver code to test above
+    print("Sorted array:", selectnsort(a))
+
+    #This code is contributed by Akhil
