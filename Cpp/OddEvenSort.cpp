c1c0b43aeeaa9b4384660ad190e3d0ebfab27841
Rohit Mazumder <30836968+RohitMazumder@users.noreply.github.com>
2020-10-02 23:40:21 +0530

    Updated indentation to 4 spaces

--- Cpp/OddEvenSort.cpp
+++ Cpp/OddEvenSort.cpp
@@ -5,62 +5,62 @@ using namespace std;
 // Returns true if the alternating elements are sorted already, otherwise false.
 bool bubbleSort(int arr[], int n, int start)
 {
-	bool isSorted = true;
+    bool isSorted = true;
 
-	for (int i = start; i <= n - 2; i = i + 2) { 
-		if (arr[i] > arr[i + 1]) { 
-			swap(arr[i], arr[i + 1]); 
-			isSorted = false; 
-		} 
-	} 
+    for (int i = start; i <= n - 2; i = i + 2) { 
+        if (arr[i] > arr[i + 1]) { 
+            swap(arr[i], arr[i + 1]); 
+            isSorted = false; 
+        } 
+    } 
 
-	return isSorted;
+    return isSorted;
 }
 
 void oddEvenSort(int arr[], int n) 
 { 
-	bool isSorted = false; 
+    bool isSorted = false; 
 
-	while (!isSorted) { 
-		isSorted = true; 
+    while (!isSorted) { 
+        isSorted = true; 
 
-		// Perform Bubble sort on odd indexed element, 
-		// which is same as performing bubble sort on every 
-		// alternating element starting from 1st element.
-		isSorted = bubbleSort(arr, n, 1);		
+        // Perform Bubble sort on odd indexed element, 
+        // which is same as performing bubble sort on every 
+        // alternating element starting from 1st element.
+        isSorted = bubbleSort(arr, n, 1);       
 
-		// Perform Bubble sort on even indexed element, 
-		// which is same as performing bubble sort on every 
-		// alternating element starting from 0th element.		
-		isSorted = bubbleSort(arr, n, 0); 
-	} 
+        // Perform Bubble sort on even indexed element, 
+        // which is same as performing bubble sort on every 
+        // alternating element starting from 0th element.       
+        isSorted = bubbleSort(arr, n, 0); 
+    } 
 
 } 
 
 void printArray(int arr[], int n) 
 { 
-	for (int i = 0; i < n; i++) 
-		cout << arr[i] << " "; 
-	cout << "\n"; 
+    for (int i = 0; i < n; i++) 
+        cout << arr[i] << " "; 
+    cout << "\n"; 
 } 
 
 // Driver program.
 int main() 
 { 
-	int n = 6, maxElement = 100;
-	int arr[n]; 
-	
-	for(int i = 0; i < n; i++) {
-		arr[i] = rand() % maxElement;
-	}
+    int n = 6, maxElement = 100;
+    int arr[n]; 
+    
+    for(int i = 0; i < n; i++) {
+        arr[i] = rand() % maxElement;
+    }
 
-	cout << "Before sorting: " << endl;
-	printArray(arr, n); 
+    cout << "Before sorting: " << endl;
+    printArray(arr, n); 
 
-	oddEvenSort(arr, n); 
-	
-	cout << "After sorting: " << endl;
-	printArray(arr, n); 
+    oddEvenSort(arr, n); 
+    
+    cout << "After sorting: " << endl;
+    printArray(arr, n); 
 
-	return 0; 
+    return 0; 
 } 
