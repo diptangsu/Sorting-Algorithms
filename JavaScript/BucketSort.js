d1e71b95404d3f4ca0c80c1cd91cebe39c25c0c5
Rishi N Desai <rishindesai@yahoo.com>
2020-10-03 00:56:08 +0530

    changed to camel case

--- JavaScript/BucketSort.js
+++ JavaScript/BucketSort.js
@@ -1,4 +1,4 @@
-const insertion_sort = bucket => {
+const insertionSort = bucket => {
     for(i=0; i < bucket.length; i++){
         const up = bucket[i]
         let j = i-1
@@ -11,35 +11,35 @@ const insertion_sort = bucket => {
     return bucket
 }
 
-const bucket_sort = initial_array => {
-    const num_buckets = 10
+const bucketSort = initialArray => {
+    const numBuckets = 10
     let array = []
-    for(let i=0; i < num_buckets; i++){
+    for(let i=0; i < numBuckets; i++){
         array.push([])
     }
 
     // put array elements in respective buckets
-    for(elem of initial_array){
-        const bucket_index = parseInt(num_buckets * elem)
-        array[bucket_index].push(elem)
+    for(elem of initialArray){
+        const bucketIndex = parseInt(numBuckets * elem)
+        array[bucketIndex].push(elem)
     }
 
     // insertion sort on buckets individually
-    for(let i=0; i < num_buckets; i++){
-        array[i] = insertion_sort(array[i])
+    for(let i=0; i < numBuckets; i++){
+        array[i] = insertionSort(array[i])
     }
 
     // concatenate the result
     let k = 0
-    for(i=0; i < num_buckets; i++){
+    for(i=0; i < numBuckets; i++){
         for(elem of array[i]){
-            initial_array[k] = elem
+            initialArray[k] = elem
             k++
         }
     }
-    return initial_array
+    return initialArray
 }
 
-const initial_array = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434]
-const sorted_array = bucket_sort(initial_array)
-console.log(sorted_array)
+const initialArray = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434]
+const sortedArray = bucketSort(initialArray)
+console.log(sortedArray)
