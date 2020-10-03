5a023a90abb22878cfe9878b44b5b204fe7f4d25
Dhruv Kachhadia <59411325+dhruv1294@users.noreply.github.com>
2020-10-03 23:52:27 +0530

    refactored shellsort

--- Cpp/ShellSort.cpp
+++ Cpp/ShellSort.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
 using namespace std;
-void swapping(int &a, int &b) {        //swap the content of a and b
+void swapping(int &a, int &b) {        
+//to swap the content of a and b
    int temp;
    temp = a;
    a = b;
    b = temp;
 }
 void display(int *array, int size) {
+//to display the elements in array
    for(int i = 0; i<size; i++)
       cout << array[i] << " ";
    cout << endl;
 }
 void shellSort(int *arr, int n) {
    int gap, j, k;
-   for(gap = n/2; gap > 0; gap = gap / 2) {        //initially gap = n/2,
+   for(gap = n/2; gap > 0; gap = gap / 2) { 
       decreasing by gap /2
       for(j = gap; j<n; j++) {
          for(k = j-gap; k>=0; k -= gap) {
@@ -27,16 +29,17 @@ void shellSort(int *arr, int n) {
 }
 int main() {
    int n;
-   cout << "Enter the number of elements: ";
+   cout << "Enter number of elements: ";
    cin >> n;
-   int arr[n];     //create an array with given number of elements
+   //create an array with given number of elements
+   int arr[n];     
    cout << "Enter elements:" << endl;
    for(int i = 0; i<n; i++) {
       cin >> arr[i];
    }
-   cout << "Array before Sorting: ";
+   cout << "Before Sorting: ";
    display(arr, n);
    shellSort(arr, n);
-   cout << "Array after Sorting: ";
+   cout << "After Sorting: ";
    display(arr, n);
 }
