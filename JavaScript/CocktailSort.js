08da6f19d9661a693d9bca72e5ccc85919f3a723
Nilesh Sharma <55238678+nilesh-05@users.noreply.github.com>
2020-10-03 13:50:32 +0530

    Spaces to 4.

--- JavaScript/CocktailSort.js
+++ JavaScript/CocktailSort.js
@@ -3,23 +3,23 @@ const cocktailSort = (arrInput) => {
 	while (isSwapped) {
 		isSwapped = false;
 		for (let i = startIndex; i < endIndex - 1; i++) {
-		if (arrInput[i] > arrInput[i + 1]) {
-			let temp = arrInput[i];
-			arrInput[i] = arrInput[i + 1];
-			arrInput[i + 1] = temp;
-			isSwapped = true;
+			if (arrInput[i] > arrInput[i + 1]) {
+				let temp = arrInput[i];
+				arrInput[i] = arrInput[i + 1];
+				arrInput[i + 1] = temp;
+				isSwapped = true;
 			}
 		}
 		endIndex--;
 		if (!isSwapped)    break;
 		isSwapped = false;
 		for (let i = endIndex - 1; i > startIndex; i--) {
-		if (arrInput[i - 1] > arrInput[i]) {
-			let temp = arrInput[i];
-			arrInput[i] = arrInput[i - 1];
-			arrInput[i - 1] = temp;
-			isSwapped = true;
-		}
+			if (arrInput[i - 1] > arrInput[i]) {
+				let temp = arrInput[i];
+				arrInput[i] = arrInput[i - 1];
+				arrInput[i - 1] = temp;
+				isSwapped = true;
+			}
 		}
 		startIndex++;
 	}
