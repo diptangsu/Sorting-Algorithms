6ae836d3865d7ffcd47b501114d5c5940047302e
Nilesh Sharma <55238678+nilesh-05@users.noreply.github.com>
2020-10-03 13:49:26 +0530

    Indent to 4 spaces

--- JavaScript/CocktailSort.js
+++ JavaScript/CocktailSort.js
@@ -1,29 +1,29 @@
 const cocktailSort = (arrInput) => {
-let startIndex = 0, endIndex = arrInput.length, isSwapped = true;
-while (isSwapped) {
-    isSwapped = false;
-    for (let i = startIndex; i < endIndex - 1; i++) {
-	if (arrInput[i] > arrInput[i + 1]) {
-		let temp = arrInput[i];
-		arrInput[i] = arrInput[i + 1];
-		arrInput[i + 1] = temp;
-		isSwapped = true;
-        }
-    }
-    endIndex--;
-    if (!isSwapped)    break;
-    isSwapped = false;
-    for (let i = endIndex - 1; i > startIndex; i--) {
-	if (arrInput[i - 1] > arrInput[i]) {
-	    let temp = arrInput[i];
-	    arrInput[i] = arrInput[i - 1];
-	    arrInput[i - 1] = temp;
-	    isSwapped = true;
+	let startIndex = 0, endIndex = arrInput.length, isSwapped = true;
+	while (isSwapped) {
+		isSwapped = false;
+		for (let i = startIndex; i < endIndex - 1; i++) {
+		if (arrInput[i] > arrInput[i + 1]) {
+			let temp = arrInput[i];
+			arrInput[i] = arrInput[i + 1];
+			arrInput[i + 1] = temp;
+			isSwapped = true;
+			}
+		}
+		endIndex--;
+		if (!isSwapped)    break;
+		isSwapped = false;
+		for (let i = endIndex - 1; i > startIndex; i--) {
+		if (arrInput[i - 1] > arrInput[i]) {
+			let temp = arrInput[i];
+			arrInput[i] = arrInput[i - 1];
+			arrInput[i - 1] = temp;
+			isSwapped = true;
+		}
+		}
+		startIndex++;
 	}
-    }
-    startIndex++;
-}
-return arrInput;
+	return arrInput;
 }
 //Cocktail Sort is a variatio of bubble sort.
 //Example Usage : 
