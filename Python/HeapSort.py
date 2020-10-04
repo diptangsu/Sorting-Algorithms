6ed50ba1b89111ca05a4edd74f60409a0162292e
diptangsu <diptangsu.97@gmail.com>
2020-10-04 20:40:02 +0530

    refactored HeapSort.py

--- Python/HeapSort.py
+++ Python/HeapSort.py
@@ -4,24 +4,21 @@
 # n is size of heap
 
 
-def heapify(arr, n, i):
-    largest = i  # Initialize largest as root
-    l = 2 * i + 1     # left = 2*i + 1
-    r = 2 * i + 2     # right = 2*i + 2
+def heapify(arr, n, root):
+    largest = root
+    left = 2 * root + 1
+    right = 2 * root + 2
 
-    # See if left child of root exists and is
-    # greater than root
-    if l < n and arr[i] < arr[l]:
-        largest = l
-
-    # See if right child of root exists and is
-    # greater than root
-    if r < n and arr[largest] < arr[r]:
-        largest = r
+    # See if left child of root exists and is greater than root
+    if left < n and arr[root] < arr[left]:
+        largest = left
+    # See if right child of root exists and is greater than root
+    if right < n and arr[largest] < arr[right]:
+        largest = right
 
     # Change root, if needed
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+    if largest != root:
+        arr[root], arr[largest] = arr[largest], arr[root]
 
         # Heapify the root.
         heapify(arr, n, largest)
@@ -36,9 +33,8 @@ def heap_sort(arr):
     for i in range(n, -1, -1):
         heapify(arr, n, i)
 
-    # One by one extract elements
     for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # swap
+        arr[i], arr[0] = arr[0], arr[i]
         heapify(arr, i, 0)
 
 
