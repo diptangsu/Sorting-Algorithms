fd0bf4b82730df8ce2a2c66faa21af0b30f17a1c
diptangsu <diptangsu.97@gmail.com>
2020-10-04 20:36:03 +0530

    refactored StoogeSort.py

--- Python/StoogeSort.py
+++ Python/StoogeSort.py
@@ -1,35 +1,26 @@
 # Python program to implement stooge sort
-def stoogesort(arr, l, h):
-    if l >= h:
+def stooge_sort(arr, first=0, last=None):
+    if last is None:
+        last = len(arr) - 1
+    if first >= last:
         return
 
-    # If first element is smaller
-    # than last, swap them
-    if arr[l] > arr[h]:
-        t = arr[l]
-        arr[l] = arr[h]
-        arr[h] = t
+    # If first element is smaller  than last, swap them
+    if arr[first] > arr[last]:
+        arr[first], arr[last] = arr[last], arr[first]
 
-    # If there are more than 2 elements in
-    # the array
-    if h - l + 1 > 2:
-        t = (h - l + 1) // 3
-
+    # If there are more than 2 elements in the array
+    if last - first + 1 > 2:
+        t = (last - first + 1) // 3
         # Recursively sort first 2 / 3 elements
-        stoogesort(arr, l, (h - t))
-
+        stooge_sort(arr, first, (last - t))
         # Recursively sort last 2 / 3 elements
-        stoogesort(arr, l + t, (h))
-
-        # Recursively sort first 2 / 3 elements
-        # again to confirm
-        stoogesort(arr, l, (h - t))
+        stooge_sort(arr, first + t, (last))
+        # Recursively sort first 2 / 3 elements again to confirm
+        stooge_sort(arr, first, (last - t))
 
 
 if __name__ == '__main__':
     arr = [2, 4, 5, 3, 1]
-    n = len(arr)
-
-    stoogesort(arr, 0, n - 1)
-
+    stooge_sort(arr)
     print(arr)
