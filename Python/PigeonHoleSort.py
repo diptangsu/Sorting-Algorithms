c576611865aaf1e9e2d405fe4e8d6d321363b4c2
diptangsu <diptangsu.97@gmail.com>
2020-10-04 20:45:14 +0530

    fixed exception

--- Python/PigeonHoleSort.py
+++ Python/PigeonHoleSort.py
@@ -13,8 +13,8 @@ def pigeonhole_sort(a):
 
     # Populate the pigeonholes.
     for x in a:
-        if type(x) is not int:
-            raise AssertionError("integers only please")
+        if not isinstance(x, int):
+            raise ValueError(f'{x} is not an integer')
         holes[x - my_min] += 1
 
     # Put the elements back into the array in order.
