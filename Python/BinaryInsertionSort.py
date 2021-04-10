a67812c63c340b27422bd1424bfc9420a8d29bc5
Shuvojit Sarkar <s15sarkar@yahoo.com>
2021-04-11 02:04:57 +0530

    Code cleanup
    Unnecessary else / elif removed

--- Python/BinaryInsertionSort.py
+++ Python/BinaryInsertionSort.py
@@ -10,8 +10,7 @@ def binary_search(arr, val, start, end):
     if start == end:
         if arr[start] > val:
             return start
-        else:
-            return start + 1
+        return start + 1
 
     # this occurs if we are moving beyond left's boundary
     # meaning the left boundary is the least position to
