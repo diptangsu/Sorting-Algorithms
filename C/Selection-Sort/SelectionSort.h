189c57be3b77648146afed07a06571a872fe643c
wassim31 <wassimbs088@gmail.com>
2021-07-30 17:12:50 +0100

    fixing the code-logic & modular design

---
+++ C/Selection-Sort/SelectionSort.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+void array_init(int *array, int SIZE);
+void array_print(int *array, int SIZE);
+void swap(int *a, int *b);
+void selection_sort(int *array, int N);
