f409a323d3538c6b709e1bf812f9976bb4b68417
SleepingNerd <79285990+SleepingNerd@users.noreply.github.com>
2022-02-26 18:07:32 +0100

    Update StalinSort.py

--- Python/StalinSort.py
+++ Python/StalinSort.py
@@ -22,6 +22,6 @@ def stalin_sort(lis):
 
 # Driver code 
 if __name__ == '__main__':
-    lis = [20,0,1,188,20,100,2,10]
+    lis = [20 ,0 ,1 ,188 ,20 ,100 ,2 ,10]
     print(f"Uncorrected list: {lis}")
     print(f"Corrected list: {stalin_sort(lis)}")
