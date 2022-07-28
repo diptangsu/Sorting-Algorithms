7f22379cf72ad83bc3d27cc93cf2c6d11e224a2c
YeezyYeezyWhatsGood <bcatalfo@gmail.com>
2022-07-28 19:01:30 -0400

    It's more pythonic to handle the edge case first then proceed with the base case- by doing so we use less indentation and get more readable code. Remember "Flat is better than nested" - The Zen of Python (type import this into a python interpreter)

--- Python/QuickSort.py
+++ Python/QuickSort.py
@@ -6,8 +6,8 @@
 # to left of pivot and all greater elements to right
 # of pivot
 def partition(arr, low, high):
-    i = (low - 1)         # index of smaller element
-    pivot = arr[high]     # pivot
+    i = low - 1  # index of smaller element
+    pivot = arr[high]  # pivot
 
     for j in range(low, high):
         # If current element is smaller than the pivot
@@ -20,6 +20,7 @@ def partition(arr, low, high):
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
     return i + 1
 
+
 # The main function that implements QuickSort
 # arr[] --> Array to be sorted,
 # low  --> Starting index,
@@ -27,19 +28,20 @@ def partition(arr, low, high):
 
 # Function to do Quick sort
 def quick_sort(arr, low, high):
-    if low < high:
-        # pi is partitioning index, arr[p] is now
-        # at right place
-        pi = partition(arr, low, high)
+    if low >= high or low < 0:
+        return
+    # pi is partitioning index, arr[p] is now
+    # at right place
+    pi = partition(arr, low, high)
 
-        # Separately sort elements before
-        # partition and after partition
-        quick_sort(arr, low, pi - 1)
-        quick_sort(arr, pi + 1, high)
+    # Separately sort elements before
+    # partition and after partition
+    quick_sort(arr, low, pi - 1)
+    quick_sort(arr, pi + 1, high)
 
 
 # Driver code to test above
-if __name__ == '__main__':
+if __name__ == "__main__":
     arr = [10, 7, 8, 9, 1, 5]
     n = len(arr)
     quick_sort(arr, 0, n - 1)
